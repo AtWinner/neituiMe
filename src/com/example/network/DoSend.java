8cd298c7043b08a778e8452276976029ca055ce2
AtWinner <361922032@qq.com>
2014-12-03 23:32:44 +0800

    获取经纬度，明天添加城市识别
    2014-12-3 23:32:40

--- src/com/example/network/DoSend.java
+++ src/com/example/network/DoSend.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class DoSend 
 {
-	public static void sendMail()
+	public static void sendMail(String Content)
 	{
 		MailSenderInfo mailInfo = new MailSenderInfo();
 		mailInfo.setMailServerHost("smtp.163.com");
@@ -13,15 +13,15 @@ public class DoSend
 		// 邮箱用户名
 		mailInfo.setUserName("hufanglin8866@163.com");
 		// 邮箱密码
-		mailInfo.setPassword("coder?1:0");
+		mailInfo.setPassword("123456a?");
 		// 发件人邮箱
 		mailInfo.setFromAddress("hufanglin8866@163.com");
 		// 收件人邮箱
-		mailInfo.setToAddress("hufanglin@inspur.com");
+		mailInfo.setToAddress("neituiMeGet@163.com");
 		// 邮件标题
-		mailInfo.setSubject("测试Java程序发送邮件");
+		mailInfo.setSubject("neituiMe用户数据");
 		// 邮件内容
-		mailInfo.setContent("邮件发送成功了咩？");
+		mailInfo.setContent(Content);
 		// 发送邮件
 		SimpleMailSender sms = new SimpleMailSender();
 		// 发送文体格式
